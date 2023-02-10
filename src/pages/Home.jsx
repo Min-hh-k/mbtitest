a0ffc8f4a205bdabd31d26fffd8d1461ca6bc995
Min Hyeok <96058996+Min-hh-k@users.noreply.github.com>
2023-02-10 17:09:39 +0900

    🍏 Pages/Home.jsx 작성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import mainCat from "../assets/mainCat.png";
+import { Button } from "react-bootstrap";
+
+function Home() {
+  return (
+    <Wrapper>
+      <Header>집사 판별기</Header>
+
+      <Contents>
+        <Title>너에게 맞는 냥이는 무엇인가?!</Title>
+        <LogoImage>
+          <img
+            src={mainCat}
+            className="rounded-circle"
+            alt="mainCat"
+            width={350}
+            height={400}
+          ></img>
+        </LogoImage>
+        <Desc>나와 잘 맞는 냥이 찾기</Desc>
+        <Button type="button" className="btn btn-secondary">테스트 시작</Button>
+      </Contents>
+    </Wrapper>
+  );
+}
+
+export default Home;
+
+const Wrapper = styled.div`
+  background-color: pink;
+  height: 100vh;
+  width: 100%;
+  font-family: 'Tenada';
+`;
+
+const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  font-size: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+`;
+
+const LogoImage = styled.div`
+  margin-top: 15px;
+`;
+
+const Desc = styled.div`
+  font-size: 20px;
+  margin-top: 20px;
+`
